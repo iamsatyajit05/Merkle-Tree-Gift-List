fc99de949ad45460a6f7fd6e82e956da96f3a9e3
Satyajit Chaudhary <thisissatyajit05@gmail.com>
2023-04-30 15:39:53 +0530

    mission accomplished

--- client/index.js
+++ client/index.js
@@ -4,11 +4,19 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const MERKLE_TREE = new MerkleTree(niceList);
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  const name = 'Edmond Carroll PhD';
+  const index = niceList.findIndex(n => n === name);
+  const proof = MERKLE_TREE.getProof(index);
+
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    name,
+    proof
   });
 
   console.log({ gift });
